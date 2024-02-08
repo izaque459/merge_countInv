c3364f3a725af87a1f106451aff6f832cdf175bb
Isaac_Santos <isaacdsantos@hotmail.com>
2024-02-08 15:30:09 -0300

    Criado e modificado arquivo count_mergesort.py

---
+++ Python/count_mergesort.py
@@ -0,0 +1,18 @@
+
+
+
+def contador_mergesort(vetor):
+    
+
+def main():
+    vetor = random.sample(range(1, 101), 100)
+    print("Vetor Aleatório: ", vetor)
+    inversoes,vetor = contador_mergesort(vetor)
+    print("Número de Inversões: ", inversoes)
+    print("Vetor Ordenado: ",vetor)
+
+
+
+
+if __name__ == "__main__":
+	main()
